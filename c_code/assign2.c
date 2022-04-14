9e596e35f7202e2a5992b6026e195acfac10e881
HarshaVardhanReddyG <102507828+HarshaVardhanReddyG@users.noreply.github.com>
2022-04-14 21:11:44 +0530

    Update and rename assign2.c to c_code/assign2.c

--- c_code/assign2.c
+++ c_code/assign2.c
@@ -5,16 +5,13 @@ int main(){
     a=b;//for verification
     int A[2][2] = { {5,a} , {b,0} }; //given matrix
     int Atran[2][2];//Transpose of A
+    //Finding Transpose of A
     for(int i=0;i<2;i++){
         for(int j=0;j<2;j++){
             Atran[i][j]=A[j][i];
         }
     }
-    /*for(int k=0;k<2;k++){
-        for(int l=0;l<2;l++){
-            printf("%d ",Atran[k][l]);
-        }
-    }*/
+    //Verifying whether A is symmetric or not
     for(int i=0;i<2;i++){
             for(int j=0;j<2;j++){
             if(A[i][j] != Atran[i][j]){
